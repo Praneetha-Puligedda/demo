d18211838d9099ac0a0ffc451dbf8cb9fa5f2eb1
Praneetha-Puligedda <129724774+Praneetha-Puligedda@users.noreply.github.com>
2024-01-12 10:33:34 +0530

    Update pranee.py

--- pranee.py
+++ pranee.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.header('puppy')
-st.write('puppy')
+st.header('panda')
+st.write('do nothing , eat noodles(●'◡'●)')
 st.image('https://i.pinimg.com/originals/37/12/4f/37124f7a0c06bd3b11aec01a7386876d.jpg',caption='eating panda')
